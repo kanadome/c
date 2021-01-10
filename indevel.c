fb61e774b65f9e8b32560843a1a1e284b54e42b2
kanadome <kanadome0ji@gmail.com>
2021-01-10 22:15:51 +0900

    2021.1.10

--- indevel.c
+++ indevel.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 
+int return_exponentiation(int, int);
+
+/*
 int main(void)
 {
-    int num;
-    int *Pnum;
-    Pnum = &num;
-
+    int num[20] = {};
     int i = 0;
-    while (i < 5)
+    for (int count = 0; count < 20; count++) {num[count] = count * count; }
+    while (i < 20) {printf("\n %x >> %x", i  + 1,num[i]); i++; }
+    printf("\n");
+    return 0;
+}
+*/
+
+int main(void)
+{
+    int x, y;
+    printf("\n  >>");
+    scanf("%d", &x);
+    printf("\n  >>");
+    scanf("%d", &y);
+
+    int result = return_exponentiation(x, y);
+
+    printf("\n\n     %d\n", result);
+
+    return 0;
+}
+
+int return_exponentiation(int x, int y)
+{
+    int result;
+    int i = 1;
+    result = x;
+    while (i < y)
     {
-        num = i;
-        printf("\nnum : %d\n", num);
-        printf("\nPnum  : %p\n", Pnum);
-        printf("\n*Pnum  : %d\n", *Pnum);
-        printf("\n --------------\n");
-
-        *Pnum  = i * i;
-        printf("%d", num);
-        printf("\n===========================\n");
-
+        result =  result * x;
         i++;
     }
+    return result;
 }
