e8dcfe42f5f5e96e4e80d925079707f52bb37cd5
zer0 <zer0@arch.localdomain>
2021-02-04 18:46:50 +0000

    2021.2.4

--- socket-s.c
+++ socket-s.c
@@ -3,10 +3,18 @@
 #include <sys/un.h>
 #include <netinet/in.h>
 
-int main(void){
+#define PORT 1992
 
-    int socket_d = socket(AF_INET, SOCK_STREAM, 0);
-    
-    
-    return 0;
+int main(){
+
+    struct sockaddr_in addr;
+    struct sockaddr_in client;
+
+    int sock0 = socket(AF_INET, SOCK_STREAM, 0);
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    unsigned int addr_len = sizeof(addr);
+    bind (sock0, &addr, addr_len);
 }
